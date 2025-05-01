b3174a8169468b198e398971d5e3d65d01c1ac0d
Lecio Ribeiro <139591377+leciojor@users.noreply.github.com>
2025-05-01 14:19:28 -0600

    init

---
+++ data.py
@@ -0,0 +1,24 @@
+from torch.utils.data import Dataset
+import torch
+import torch.nn as nn
+
+
+
+class PlaysDataset(Dataset):
+
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        pass
+        #return x, label
+
+    def correlation_analysis(self):
+        pass
+
+
+
+
