8f730c0cc99b6dc0fda0f12cac6c0fdcf9102a57
julisud <matheusbyui@gmail.com>
2025-05-12 16:18:33 -0600

    commentendo pra pull

--- data.py
+++ data.py
@@ -20,9 +20,8 @@ class PlaysData(Dataset):
 
         self.initializing_df_data()
         self.process_plays()
-        self.add_labels()
 
-        pd.DataFrame.from_dict(self.data)
+        self.data = pd.DataFrame.from_dict(self.data)
         self.converting_numerical()
         
 
@@ -58,7 +57,7 @@ class PlaysData(Dataset):
         self.data["qb_speed"] = []
         self.data["qb_direction"] = []
         self.data["qb_accel"] = []
-        self.data["qb_result"] = []
+        self.data["result"] = []
         for i in range(6):
             self.data[f"qb_pressure_{i}"] = []
         
@@ -89,13 +88,16 @@ class PlaysData(Dataset):
                 self.data["qb_speed"].append(qb_snap['s'])
                 self.data["qb_direction"].append(qb_snap['dir'])
                 self.data["qb_accel"].append(qb_snap['a'])
+                
+                targetedReceiver = None
+                self.receivers = play_players[play_players['routeRan'].notna()].copy()
+                # May need to be optimized
+                self.receivers = self.receivers.merge(self.players[['nflId', 'position']], on='nflId', how='left')
+                self.sorting_receivers(play_df, ball_snap_frame)
 
-                self.data["qb_result"].append(play_info['passResult'])
-
-                receivers = play_players[play_players['routeRan'].notna()].head(5)
                 for i in range(5):
-                    if i < len(receivers):
-                        r = receivers.iloc[i]
+                    if i < len(self.receivers):
+                        r = self.receivers.iloc[i]
                         rid = r['nflId']
                         r_data = play_df[play_df['nflId'] == rid].sort_values('frameId')
                         if not r_data.empty:
@@ -107,9 +109,11 @@ class PlaysData(Dataset):
                             self.data[f"orientation_{i}"].append(r_snap['o'])
                             dist = ((r_snap['x'] - qb_snap['x']) ** 2 + (r_snap['y'] - qb_snap['y']) ** 2) ** 0.5
                             self.data[f"dist_qb_{i}"].append(dist)
-                            self.data[f"receiver_type_{i}"].append(play_df['position'])
+                            self.data[f"receiver_type_{i}"].append(r['position'])
+                            if r["wasTargettedReceiver"]:
+                                targetedReceiver = i
 
-                            defenders = play_df[play_df['position'].isin(['CB', 'S', 'LB', 'FS', 'SS', 'DE', 'DT'])]
+                            defenders = play_df[play_df['position'].isin(['CB', 'S', 'LB', 'FS', 'SS', 'DE', 'DT'])].copy()
                             defenders['dist'] = ((defenders['x'] - r_snap['x']) ** 2 +
                                                 (defenders['y'] - r_snap['y']) ** 2) ** 0.5
                             closest = defenders.nsmallest(2, 'dist')
@@ -171,17 +175,33 @@ class PlaysData(Dataset):
                 self.data["qb_pressure_2"].append(caused_pressure)
                 self.data["qb_pressure_5"].append(time_to_pressure or 0)
 
-    def add_labels(self):
-        pass
+                if self.v == 1 or self.v == 4:
+                    self.data["result"].append(targetedReceiver)
+                elif self.v == 2:
+                    self.data["result"].append(play_df["dis"])
+                elif self.v == 3:
+                    self.data["result"].append(play_info["passResult"])
+                print(self.data)
+    def sorting_receivers(self, play_df, ball_snap_frame):
+        snap_frame = play_df[play_df['frameId'] == ball_snap_frame]
+        receiver_positions = snap_frame[snap_frame['nflId'].isin(self.receivers['nflId'])][['nflId', 'y']]
+
+        self.receivers = self.receivers.merge(receiver_positions, on='nflId', how='left')
+
+        self.receivers = self.receivers.sort_values(by='y', ascending=True).head(5)
+        
 
     def converting_numerical(self):
         pass
 
     def correlation_analysis(self):
         pass
+    
+    def augmentation(self):
+        pass
 
     def get_csv(self):
-        self.data.to_csv("final_data.csv", index=False)
+        self.data.to_csv("/final_data.csv", index=False)
 
 
 
