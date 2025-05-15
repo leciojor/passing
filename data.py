0480b8fd8c3484c19c91718d8c1d8d126d4dfc82
Lecio Ribeiro <139591377+leciojor@users.noreply.github.com>
2025-05-14 20:18:58 -0600

    new adjustment to results

--- data.py
+++ data.py
@@ -154,13 +154,13 @@ class PlaysData(Dataset):
                                             self.data[f"defensor_{field}_{j}_{k}"].append(None)
                             else:
                                 for field in ['x', 'y', 'vel', 'accel', 'orientation', 'dist_qb', 'receiver_type']:
-                                    self.data[f"{field}_{k}"].append(None)
+                                    self.data[f"{field}_{j}"].append(None)
                                 for k in range(2):
                                     for field in ['x', 'y', 'vel', 'accel', 'orientation']:
                                         self.data[f"defensor_{field}_{j}_{k}"].append(None)
                         else:
                             for field in ['x', 'y', 'vel', 'accel', 'orientation', 'dist_qb', 'receiver_type']:
-                                self.data[f"{field}_{k}"].append(None)
+                                self.data[f"{field}_{j}"].append(None)
                             for k in range(2):
                                 for field in ['x', 'y', 'vel', 'accel', 'orientation']:
                                     self.data[f"defensor_{field}_{j}_{k}"].append(None)
