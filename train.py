b4e5b57b06fddc556e8a2a094129aa871de8713c
= <leciojoaoorsiribeiro@gmail.com>
2025-05-16 14:29:37 -0600

    training logic bug

--- train.py
+++ train.py
@@ -21,13 +21,13 @@ lr = 0.01
 #training variant 5
 
 train_loader, val_loader, dataset = getting_loader(16, save=False, num_workers=0, variant = 5, train_p=0.8, saved=True, distr_analysis=False, get_dataset=True)
-net = DeepQBVariant1(input_dim=len(dataset.columns), output_dim=1)
+net = DeepQBVariant1(input_dim=len(dataset.data.columns), output_dim=1)
 optimizer = torch.optim.Adam(lr=lr, params=net.parameters())
 loss_training, acc_training, loss_val, acc_val = train(net, optimizer, nn.BCEWithLogitsLoss(), train_loader, val_loader, n, 1, 1, None, f"variant5_lr:{lr}_n:{n}", t=5, pretrained=False)
 
 #training variant 6
 
 train_loader, val_loader, dataset = getting_loader(16, save=False, num_workers=0, variant = 6, train_p=0.8, saved=True, distr_analysis=False, get_dataset=True)
-net = DeepQBVariant1(input_dim=len(dataset.columns), output_dim=3)
+net = DeepQBVariant1(input_dim=len(dataset.data.columns), output_dim=3)
 optimizer = torch.optim.Adam(lr=lr, params=net.parameters())
 loss_training, acc_training, loss_val, acc_val = train(net, optimizer, nn.CrossEntropyLoss(), train_loader, val_loader, n, 1, 1, None,f"variant6:{lr}_n:{n}", t=6, pretrained=False)
